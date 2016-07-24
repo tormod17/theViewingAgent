a5deda44544019ac69e518f78a5826e9f416e96f
tormod17 <tormodsmith@gmail.com>
2016-07-24 20:21:52 +0100

    new ajax request

--- js/app.js
+++ js/app.js
@@ -43,61 +43,8 @@ var listings;
 
     
 function getListings (location,bedroom_max) { 
-            
-            var request = {  
-              area: location,            
-              api_key: 'aejnbvt9axw4b5upkb2ccwnk'
-            } 
-
-            var response = $.ajax({
-            
-            url: "http://api.zoopla.co.uk/api/v1/property_listings.json",
-            data: request,
-            dataType: "json",
-            type: "GET",
-            
-            })
-           
-
-            .done(function(response){
-                var nestoriaLink= response.response.link_to_url;
-                console.log('>>>>>>>>>>>>',nestoriaLink, response)
-    
-                var listings = response.response.listings;
-                var No_of_listings = listings.length;
-                console.log(listings);
-                        
-                        var totalPrice=0;
-                        var totalComm=0;
-                        var agentComm=0;
-                        var averagePrice=0;
-                        var averageComm=0;
-
-                  $.each(listings, function(i, item) {
-                      
-                        var listingPrice = item.price;
-                            totalPrice+=listingPrice;
-                            agentComm = parseInt((listingPrice * 52) * 0.07);
-                            totalComm += agentComm;
-                            averageComm = parseInt(totalComm / No_of_listings);
-                            averagePrice =parseInt(totalPrice /No_of_listings);                      
-
-                  })     
-
-            $('.nestoriaLink').append('<p>' + '<a href =' + nestoriaLink+ '>' +'Click here to see the full list of properties in the selected area.</a></p>');
-            $('.results').append('<p>  Number of bedrooms:' + ' ' + bedroom_max +' '+ 'Average weekley rent: £' + averagePrice );
-            $('.hsAgentComm').append('<p> Average agent commisson :' +' ' + '£'+ averageComm  +' </p>');
-            $('.ourPrice').append('<p> The viewing agent will cost you £400 </p>');
-                  console.log(parseInt(averagePrice));
-                  console.log(parseInt(averageComm));        
-            })
-            
-            .fail(function(jqXHR, error, errorThrown){
-            var errorElem = showError(error);
-            $('.search-results').append(errorElem);
-             });
-
-
+            xhttp.open("GET", " http://api.zoopla.co.uk/api/v1/property_listings.json?area=putney&api_key=aejnbvt9axw4b5upkb2ccwnk", true);
+            xhttp.send();
 };        
 
  
